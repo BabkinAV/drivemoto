c68ae55d92e6ddbd9ef0c0dd97fb363e5b43c933
Raybeck <babkin.andrey@gmail.com>
2021-02-24 11:37:24 +0300

    added popular products section

--- js/main.js
+++ js/main.js
@@ -9,13 +9,22 @@ $(function(){
 
         e.preventDefault();
 
-        $('.tab').removeClass('tab--active');
-        $('.tabs-content').removeClass('tabs-content--active');
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
         $(this).addClass('tab--active');
         $($(this).attr('href')).addClass('tabs-content--active');
 
     });
 
-    $('.product-slider').slick({});
+    $('.product-item__favorite').on('click', function() {
+        $(this).toggleClass('product-item__favorite--active')
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>', 
+    });
 });
